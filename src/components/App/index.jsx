4345ef205417e7e63d87035d1fadaf26f9af9798
Elanis <elanis@hotmail.com>
2024-05-31 14:04:36 +0200

    Base Layout

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,15 @@
+import Footer from '../Footer';
+import Header from '../Header';
+import Home from '../Home';
+
 import './index.css';
 
 export default function App() {
 	return (
-		<div className="app">Bonjour !</div>
+		<>
+			<Header />
+			<Home />
+			<Footer />
+		</>
 	);
 }

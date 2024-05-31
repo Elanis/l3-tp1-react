200e013f48dfd73b07182c4be8a55640354936fb
Elanis <elanis@hotmail.com>
2024-05-31 14:09:10 +0200

    Iteration v1

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,23 @@
 export default function Home() {
+	const data = [
+		'Lorem',
+		'Ipsum',
+		'Dolor'
+	];
+
+	const components = [];
+	for(const element of data) {
+		components.push(
+			<li>{element}</li>
+		);
+	}
+
 	return (
-		<div className="app">Bonjour !</div>
+		<>
+			<div className="app">Bonjour !</div>
+			<ul>
+				{components}
+			</ul>
+		</>
 	);
 }
